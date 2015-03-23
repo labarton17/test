61fcb14606bff5733ca6a4e3945618a7b5219dba
laura-barton <laura.barton@gree.net>
2015-03-22 18:04:27 -0700

    Removed merge conflict

--- MyFile.c
+++ MyFile.c
@@ -1,12 +1,8 @@
 #include <stdio.h>
 
 int main() {
-<<<<<<< HEAD
     //This is a comment
-	std::string name = "Ilana";
-=======
 	std::string name = "Krysia!!!";
->>>>>>> FETCH_HEAD
 	printf("Hello World %s", name);
 	
 	return 0;
